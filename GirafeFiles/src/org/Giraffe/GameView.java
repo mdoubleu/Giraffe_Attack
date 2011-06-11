764d4053f4e160f38e73257291015a323a95836a
Michael Wineburg <mikedoubleyouu@dyn-129-64-202-144.wireless.brandeis.edu>
2011-06-11 15:36:53 -0400

    winesCommit

--- GirafeFiles/src/org/Giraffe/GameView.java
+++ GirafeFiles/src/org/Giraffe/GameView.java
@@ -10,7 +10,9 @@ import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 
 import android.os.Handler;
+import android.text.format.Time;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -22,6 +24,7 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
 	int G_y2;
 	int G_x1;
 	int G_x2;
+	int b_round;
 	public GameView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		//lets us stay updated about changes.
@@ -31,12 +34,10 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
 		//calls the inner class
 		gameThread=new GameThread(holder, context,new Handler());
        
-		//
 		G_y1=150;
         G_y2=450;
         G_x1=0;
         G_x2=150;
-		
         setFocusable(true);
 		
 	}
@@ -51,7 +52,7 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
 		int w_adapt;
 		int h_adapt;
 		boolean G_run;
-
+		long time;
 		
 		
 		   public GameThread(SurfaceHolder surfaceHolder, Context context,
@@ -59,20 +60,39 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
 			   G_surface=surfaceHolder;
 			   G_context=context;
 			   G_handler=handler;
-			   
-			   Resources res = context.getResources();
+			   b_round=0;
+			   Resources res = G_context.getResources();
 	            giraffe = context.getResources().getDrawable(
 	                    R.drawable.giraffe);
-	            background=BitmapFactory.decodeResource(res, R.drawable.a);
-	            //			Rect(1,2,3,4)- 1-how far over 2-how far down on screen 3=width 4=length
-
+	            background=BitmapFactory.decodeResource(res, R.drawable.a);	
 	            setFocusable(true);
+	            
 		   }
 		public void doDraw(Canvas canvas){
+			Resources res = G_context.getResources();	
 			canvas.drawBitmap(background, 0, 0, null);
 			giraffe.setBounds(G_x1, G_y1, G_x2, G_y2);
 			giraffe.draw(canvas);
+			canvas.save();
 			
+			if(b_round==0){
+				 background=BitmapFactory.decodeResource(res, R.drawable.a);
+				 giraffe = G_context.getResources().getDrawable(
+		                    R.drawable.giraffe2);
+			 }else if(b_round==1){
+				 background=BitmapFactory.decodeResource(res, R.drawable.a3);
+				 giraffe = G_context.getResources().getDrawable(
+		                    R.drawable.giraffe);
+			 }else if(b_round==2){
+				 background=BitmapFactory.decodeResource(res, R.drawable.a2);
+			 }else{
+				 Log.d("change", "Background");
+			 }
+			 time=System.currentTimeMillis();
+			 while(time+500>System.currentTimeMillis()){
+			 }
+			
+			canvas.restore();
 			
 		}
 		 
@@ -92,12 +112,17 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
 			
 		}
 		 public void run() {
-	            while (G_run) {
-	                Canvas c = null;
+			 	while (G_run) {
+	                time=System.currentTimeMillis();
+			 		Canvas c = null;
 	                try {
 	                    c = G_surface.lockCanvas(null);
 	                    synchronized (G_surface) {
-	                        doDraw(c);
+	                    	if(b_round==2){
+	                    		b_round=-1;
+	                    	}
+	                    	b_round++;
+	                    	doDraw(c);
 	                    } 
 	                } finally {
 	                    // do this in a finally so that if an exception is thrown
@@ -109,60 +134,73 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
 	                }
 	            }
 	        }
-
-
+		 private void changeBackground(int x){
+			 Resources res = G_context.getResources();	
+			 if(x==0){
+				background=BitmapFactory.decodeResource(res, R.drawable.a2);
+			 }else if(x==1){
+				 background=BitmapFactory.decodeResource(res, R.drawable.a2);
+				 invalidate();
+			 }else if(x==2){
+				 background=BitmapFactory.decodeResource(res, R.drawable.a3);
+			 }else{
+				 Log.d("change", "Background");
+			 }
+		}
 	}
  
+	
 public boolean onKeyUp(int keyCode, KeyEvent msg) {
-    if(keyCode==KeyEvent.KEYCODE_DPAD_UP){
-    	try {
-			jump(G_y1, G_y2);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    }else{
-    	
-    }
-    return true;
-    
-}
+	    if(keyCode==KeyEvent.KEYCODE_DPAD_UP){
+	    	try {
+				jump(G_y1, G_y2);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	    }
+	    return false;
+	    
+	}
  public boolean jumping=false;
  long cTime;//current time
  long timer;//the timer as it goes along.
-
-//Edited by Jeremy Croll
- public void jump(int y1, int y2) throws InterruptedException{
-
+ public void down(int y1, int y2){
+	 G_y1=y1+10;
+	 G_y2=y2+10;
 	 invalidate();
-	 
-	 jumping = true;
-      cTime = System.currentTimeMillis();
-      timer=System.currentTimeMillis();
-
-      //Giraffe Jump Part1
-      while((cTime+1000) > timer){  //moves 20 pixils up     
-          timer = System.currentTimeMillis(); //updates 
-          G_y1-=10;
-          G_y2-=10;
-          gameThread.sleep(50);
-          invalidate();
-      }
-          cTime = System.currentTimeMillis();
-      
-      while((cTime+1000) > timer){  //moves 20 pixils up      
-          timer = System.currentTimeMillis();
-          G_y1+=10;
-          G_y2+=10;
-          gameThread.sleep(50);
-          invalidate();
-           
-      }
-      jumping = false;
-      invalidate();
-     
  }
- 
+
+	 public void jump(int y1, int y2) throws InterruptedException{
+		 //redraws
+		 invalidate();
+
+		 jumping = true;
+	      cTime = System.currentTimeMillis();
+	      timer=System.currentTimeMillis();
+
+	      //Giraffe Jump Part1
+	      while((cTime+1000) > timer){  //moves 20 pixils up     
+	          timer = System.currentTimeMillis(); //updates 
+	          G_y1-=10;
+	          G_y2-=10;
+	          gameThread.sleep(50);
+	          invalidate();
+	      }
+	          cTime = System.currentTimeMillis();
+	      
+	      while((cTime+1000) > timer){  //moves 20 pixils up      
+	          timer = System.currentTimeMillis();
+	          G_y1+=10;
+	          G_y2+=10;
+	          gameThread.sleep(50);
+	          invalidate();
+	           
+	      }
+	      jumping = false;
+	      invalidate();
+	     
+	 }
+	 
 	public GameThread getThread(){
 		return gameThread;
 	}
@@ -179,6 +217,15 @@ public boolean onKeyUp(int keyCode, KeyEvent msg) {
 	}
 
 	public void surfaceDestroyed(SurfaceHolder holder) {
-		
+		 boolean retry = true;
+	        gameThread.setRunning(false);
+	        while (retry) {
+	            try {
+	                gameThread.join();
+	                retry = false;
+	            } catch (InterruptedException e) {
+	            }
+	        }
+
+		}
 	}
-}
